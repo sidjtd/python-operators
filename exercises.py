60eb08cc7d02735343355103ae8918abd11ca699
Victor <viclee02@gmail.com>
2018-05-06 22:14:25 -1000

    adds exercises

---
+++ exercises.py
@@ -0,0 +1,63 @@
+#Declare two variables num1 and num2 and assign it a number value of your choosing.
+
+#1. Addition
+#Add the two variables and store it to a new variable named sum.
+#Print the sum variable.
+
+#2 Subtraction
+#Subtract the two variables and store it to a new variable named difference. 
+#Print the difference variable.
+
+#3 Multiplication
+#Multiple the two variables and store it to a new variable named product. 
+#Print the product variable.
+
+#4 Division
+#Divide the two variables and store it to a new variable named quotient. 
+#Print the quotient variable.
+
+#5 Modulus
+#Find the modulus of the two variables and store it to a new variable named remainder.
+#Print the remainder variable.
+
+#6 Exponent
+#Find the square root of the num1 and store it to a new variable named square_root. 
+#Print the square_root variable.
+
+#7 Floor Division
+#Divide the two variables to find the rounded down integer and store it to a new variable named int_div.
+#Print the int_div variable.
+
+#8 Comparison and Logic
+#Declare a variable named is_equal and compare two strings 'Tacocat' and 'tacocat' using the equality (==) operator.
+#Print the is_equal variable.
+
+#9 Declare a variable named not_equal and compare the string value of '8' with the number value 8 using the inequality (!=) operator. 
+#Print the not_equal variable.
+
+#10 Assignment
+#Declare a variable named num3 and assign it a number of your choosing.
+
+#Increment and assign 5 to the num3 variable.
+#Print your results.
+
+#Decrement and assign 7 to the num3 variable.
+#Print your results.
+
+#Multiple and assign 3 to the num3 variable.
+#Print your results.
+
+#Divide and assign 2 to the num3 variable.
+#Print your results.
+
+#Modulus and assign 8 to the num3 variable.
+#Print your results.
+
+#Exponent and assign 3 to the num3 variable.
+#Print your results.
+
+#Floor divide and assign 4 to the num3 variable.
+#Print your results.
+
+
+
