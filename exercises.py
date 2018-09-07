7fbb5877f525f2775da0e1169a778a379d36dc4a
Sidney Dodson <siddodson@gmail.com>
2018-09-06 19:24:31 -1000

    Homework

--- exercises.py
+++ exercises.py
@@ -1,63 +1,107 @@
 #Declare two variables num1 and num2 and assign it a number value of your choosing.
 
+num1 = 10
+num2 = 20
+
 #1. Addition
 #Add the two variables and store it to a new variable named total.
 #Print the total variable.
 
+total = num1 + num2
+print(total)
+
 #2 Subtraction
-#Subtract the two variables and store it to a new variable named difference. 
+#Subtract the two variables and store it to a new variable named difference.
 #Print the difference variable.
 
+diff = num1 - num2
+print(diff)
+
 #3 Multiplication
-#Multiple the two variables and store it to a new variable named product. 
+#Multiple the two variables and store it to a new variable named product.
 #Print the product variable.
 
+product = num1 * num2
+print(product)
+
 #4 Division
-#Divide the two variables and store it to a new variable named quotient. 
+#Divide the two variables and store it to a new variable named quotient.
 #Print the quotient variable.
+
+quotient = num1 / num2
+print(quotient)
 
 #5 Modulus
 #Find the modulus of the two variables and store it to a new variable named remainder.
 #Print the remainder variable.
 
+remainder = num1 % num2
+print(remainder)
+
 #6 Exponent
-#Find the square of the num1 and store it to a new variable named double. 
+#Find the square of the num1 and store it to a new variable named double.
 #Print the double variable.
+
+double = num1 ** 2
+print(double)
 
 #7 Floor Division
 #Divide the two variables to find the rounded down integer and store it to a new variable named int_div.
 #Print the int_div variable.
 
+int_div = num1 // num2
+print(int_div)
+
 #8 Comparison and Logic
 #Declare a variable named is_equal and compare two strings 'Tacocat' and 'tacocat' using the equality (==) operator.
 #Print the is_equal variable.
 
-#9 Declare a variable named not_equal and compare the string value of '8' with the number value 8 using the inequality (!=) operator. 
+is_equal = ('Tacocat' == 'tacocat')
+print(is_equal)
+
+#9 Declare a variable named not_equal and compare the string value of '8' with the number value 8 using the inequality (!=) operator.
 #Print the not_equal variable.
+not_equal = '8' != 8
+print(not_equal)
 
 #10 Assignment
 #Declare a variable named num3 and assign it a number of your choosing.
+num3 = 5
+print(num3)
 
 #Increment and assign 5 to the num3 variable.
 #Print your results.
+num3 += 5
+print(num3)
 
 #Decrement and assign 7 to the num3 variable.
 #Print your results.
+num3 -= 7
+print(num3)
 
 #Multiple and assign 3 to the num3 variable.
 #Print your results.
+num3 *= 3
+print(num3)
 
 #Divide and assign 2 to the num3 variable.
 #Print your results.
+num3 /= 2
+print(num3)
 
 #Modulus and assign 8 to the num3 variable.
 #Print your results.
+num3 %= 8
+print(num3)
 
 #Exponent and assign 3 to the num3 variable.
 #Print your results.
+num3 **= 3
+print(num3)
 
 #Floor divide and assign 4 to the num3 variable.
 #Print your results.
-
+num3 //= 4
+print(num3)
 
 
